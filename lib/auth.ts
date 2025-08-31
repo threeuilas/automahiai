1ada52cdd00e84de9f08f43c44f3a0daf00067e7
Kailani Chu <me@kailani.io>
2025-08-30 19:54:21 -1000

    Session test

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from './db';
 import { createAuthClient } from 'better-auth/client';
+import { nextCookies } from 'better-auth/next-js';
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
@@ -10,6 +11,7 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
+    plugins: [nextCookies()] // make sure nextCookies is the last plugin in the array
 });
 
 export const { signIn, signUp, useSession } = createAuthClient();
