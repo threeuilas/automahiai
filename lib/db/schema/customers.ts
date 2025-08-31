7278b7842548fc4cf7a7cb6c177d7625ba0a9bb4
Kailani Chu <me@kailani.io>
2025-08-30 18:02:00 -1000

    Linter changes

--- lib/db/schema/customers.ts
+++ lib/db/schema/customers.ts
@@ -1,7 +1,7 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
-import { timestamps } from "./helpers";
+import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import { timestamps } from './helpers';
 
-export const customersTable = pgTable("customers", {
+export const customersTable = pgTable('customers', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
