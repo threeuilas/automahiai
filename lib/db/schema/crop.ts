aa163e57b85235693cabdd3c43c8f0f956fbe956
Kailani Chu <me@kailani.io>
2025-09-06 00:23:44 -1000

    Lint

--- lib/db/schema/crop.ts
+++ lib/db/schema/crop.ts
@@ -1,4 +1,4 @@
-import { integer, pgTable, pgEnum, text, jsonb } from 'drizzle-orm/pg-core';
+import { integer, pgTable, text, jsonb } from 'drizzle-orm/pg-core';
 import { relations } from 'drizzle-orm';
 import { timestamps } from './helpers';
 import { farm } from './farm';
