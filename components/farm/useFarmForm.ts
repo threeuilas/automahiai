24aa1ba2d7f484d641f863e57fb21ce9a1d2011e
Kailani Chu <me@kailani.io>
2025-09-05 20:33:27 -1000

    Lint

--- components/farm/useFarmForm.ts
+++ components/farm/useFarmForm.ts
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { SubmitErrorHandler, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 
