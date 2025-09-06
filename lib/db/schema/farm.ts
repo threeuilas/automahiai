216d4b4efa18e6aebdfa963e03a7aaefaf98e340
Kailani Chu <me@kailani.io>
2025-09-05 23:36:14 -1000

    Add initial crop schema

--- lib/db/schema/farm.ts
+++ lib/db/schema/farm.ts
@@ -1,8 +1,17 @@
 import { integer, pgTable, text } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
 import { timestamps } from './helpers';
+import { farmUser } from './farmUser';
+import { farmCrop } from './crop';
 
 export const farm = pgTable('farm', {
   id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
   name: text('name').notNull(),
   ...timestamps,
 });
+
+// Drizzle relations for farms
+export const farmRelations = relations(farm, ({ many }) => ({
+  farmUser: many(farmUser),
+  farmCrop: many(farmCrop),
+}));
